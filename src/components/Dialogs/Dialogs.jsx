3de0694e5e95660013dc0ca32ee04d6adfb508e6
Alex <olexiineiculov@gmail.com>
2022-10-13 17:59:54 +0200

    little modified dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import cl from './Dialogs.module.css';
 
 function Dialogs() {
   return (
-    <div>
-      Dialogs
+    <div className={cl.dialogs}>
+      <div className={cl.dialogsContainer}>
+        <div className={cl.dialog}>
+          Artem
+        </div>
+        <div className={cl.dialog}>
+          Dima
+        </div>
+        <div className={cl.dialog}>
+          Maxim
+        </div>
+      </div>
+      <div className={cl.MessagesContainer}>
+        <div className={cl.message}>
+          Hi
+        </div>
+        <div className={cl.message}>
+          How are you?
+        </div>
+        <div className={cl.message}>
+          I am fine
+        </div>
+      </div>
     </div>
   );
 }
